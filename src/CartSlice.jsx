83d203c919cb961813a62953fbb05f86516a1437
peemtanapat <pm.tanapat@gmail.com>
2025-06-09 12:37:45 +0700

    feat: ProductList add items to cart

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const newItem = { ...action.payload, quantity: 1 };
+      state.items.push(newItem);
     },
     removeItem: (state, action) => {
+      const toRemoveItemName = action.payload.name;
+      state.items = state.items.filter((item) => item.name != toRemoveItemName);
     },
     updateQuantity: (state, action) => {
+      const quantity = action.payload.quantity;
+      const targetItemName = action.payload.name;
 
-    
+      state.items = state.items.map((item) => {
+        if (item.name === targetItemName) {
+          return { ...item, quantity };
+        }
+      });
     },
   },
 });
